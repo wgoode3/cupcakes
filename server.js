cb0c0254601b6d883742d17036346f6bf66561cd
William Goode <wgoode3@gmail.com>
2019-12-11 15:34:27 -0500

    we've gotten pretty far

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express'),
          port = 1337,
           app = express(),
+           bp = require('body-parser'),
       DB_NAME = "cupcakes_are_cool";
 
-app.use(express.json());
+app.use(bp.json({limit: '50mb', extended: true}));
+// app.use(bp({limit: '5mb'}));
 app.use(express.static(__dirname + "/client/dist/client"));
 
 require('./server/utils/mongoose')(DB_NAME);
